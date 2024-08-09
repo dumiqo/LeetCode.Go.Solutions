27509b308a506b71da0bae78238a134f31919050
dumiqo <konstantin.kim.mail@gmail.com>
2024-08-09 20:08:15 +0300

    add Magic Squares In Grid

--- 840-Magic-Squares-In-Grid/Magic Squares In Grid.go
+++ 840-Magic-Squares-In-Grid/Magic Squares In Grid.go
@@ -1,5 +1,97 @@
 package leetcode
 
 func numMagicSquaresInside(grid [][]int) int {
+	if len(grid) < 3 || len(grid[0]) < 3 {
+		return 0
+	}
 
+	initialize(grid)
+
+	result := 0
+	if isMagic() {
+		result = 1
+	}
+	for hasMore() {
+		next()
+		if isMagic() {
+			result++
+		}
+	}
+	return result
+}
+
+var square [][]int
+var x int
+var y int
+var endX int
+var endY int
+var array []int
+
+func initialize(grid [][]int) {
+	square = grid
+	x = 0
+	y = 0
+	array = make([]int, 16)
+
+	for i := 0; i < 3; i++ {
+		for j := 0; j < 3; j++ {
+			array[square[i][j]]++
+		}
+	}
+
+	endX = len(square[0]) - 3
+	endY = len(square) - 3
+}
+
+func next() {
+	if x != endX {
+		for i := 0; i < 3; i++ {
+			array[square[y+i][x]]--
+			array[square[y+i][x+3]]++
+		}
+		x++
+	} else {
+		array = make([]int, 16)
+		x = 0
+		y++
+		for i := 0; i < 3; i++ {
+			for j := 0; j < 3; j++ {
+				array[square[y+i][j]]++
+			}
+		}
+	}
+}
+
+func hasMore() bool {
+	return x != endX || y != endY
+}
+func isMagic() bool {
+	for i := 1; i < 10; i++ {
+		if array[i] != 1 {
+			return false
+		}
+	}
+
+	for i := 0; i < 3; i++ {
+		row := square[y+i][x] + square[y+i][x+1] + square[y+i][x+2]
+		if row != 15 {
+			return false
+		}
+		col := square[y][x+i] + square[y+1][x+i] + square[y+2][x+i]
+		if col != 15 {
+			return false
+		}
+	}
+
+	diagonal := square[y][x] + square[y+1][x+1] + square[y+2][x+2]
+	if diagonal != 15 {
+		return false
+	}
+
+	diagonal = square[y][x+2] + square[y+1][x+1] + square[y+2][x]
+	if diagonal != 15 {
+		return false
+	}
+
+	return true
 }
--- 840-Magic-Squares-In-Grid/Magic Squares In Grid_test.go
+++ 840-Magic-Squares-In-Grid/Magic Squares In Grid_test.go
@@ -11,6 +11,7 @@ func TestNumMagicSquaresInside(t *testing.T) {
 		{"One magic square", [][]int{{4, 3, 8, 4}, {9, 5, 1, 9}, {2, 7, 6, 2}}, 1},
 		{"One element array input", [][]int{{8}}, 0},
 		{"Zero magic square", [][]int{{7, 0, 5}, {2, 4, 6}, {3, 8, 1}}, 0},
+		{"One magic square, in big array", [][]int{{3, 2, 9, 2, 7}, {6, 1, 8, 4, 2}, {7, 5, 3, 2, 7}, {2, 9, 4, 9, 6}, {4, 3, 8, 2, 5}}, 1},
 	}
 	// The execution loop
 	for _, tt := range tests {
