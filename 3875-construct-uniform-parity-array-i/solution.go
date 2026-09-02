b33b8d0e6e644f48edec482084c792bb41c80c97
Konstantin Kim <konstantin.kim.mail@gmail.com>
2026-09-02 10:30:20 +0300

    3875 optimize solution

--- 3875-construct-uniform-parity-array-i/solution.go
+++ 3875-construct-uniform-parity-array-i/solution.go
@@ -1,61 +1,27 @@
 package leetcode
 
 func uniformArray(nums1 []int) bool {
-	evenChan := make(chan bool)
-	oddChan := make(chan bool)
-
-	go isEven(nums1, evenChan)
-	go isOdd(nums1, oddChan)
-
-	even := <-evenChan
-	odd := <-oddChan
-
-	return even || odd
-}
-
-func isEven(nums1 []int, done chan<- bool) {
+	allOdd, allEvent := true, true
 	for i := range nums1 {
-		if nums1[i]%2 == 0 {
-			continue
-		}
-		haveEven := false
+		haveEven := nums1[i]%2 == 0
+		haveOdd := nums1[i]%2 != 0
 		for j := range nums1 {
 			if i == j {
 				continue
 			}
+			if haveEven == haveOdd && haveOdd == true {
+				break
+			}
 
-			if (nums1[i]-nums1[j])%2 == 0 {
+			if haveEven == false && (nums1[i]-nums1[j])%2 == 0 {
 				haveEven = true
-				break
 			}
-		}
-		if !haveEven {
-			done <- false
-		}
-	}
-	done <- true
-}
-
-func isOdd(nums1 []int, done chan<- bool) {
-	for i := range nums1 {
-		if nums1[i]%2 != 0 {
-			continue
-		}
-		haveOdd := false
-		for j := range nums1 {
-			if i == j {
-				continue
-			}
-
-			if (nums1[i]-nums1[j])%2 != 0 {
+			if haveOdd == false && (nums1[i]-nums1[j])%2 != 0 {
 				haveOdd = true
-				break
 			}
 		}
-		if !haveOdd {
-			done <- false
-		}
+		allOdd = haveOdd
+		allEvent = haveEven
 	}
-	done <- true
-
+	return allOdd || allEvent
 }
